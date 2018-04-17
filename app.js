7b24c3c5f20730ea5dc97bcf2ebb2e6949f66007
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-17 14:43:04 -0500

    tech stack

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const pug = require('pug');
-const { students } = require("./class");
+const { students } = require("./data/class");
+const { techStack } = require('./data/tech');
 
 app.set('view engine', 'pug');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index', {
-    students: students
-  });
+  res.render('index', { students, techStack });
 });
 
 app.listen('8080', () => {
